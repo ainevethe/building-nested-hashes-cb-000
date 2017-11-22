58fc2026b56187e772abf137642538104ab11c40
Crista Cooper <aine3d@gmail.com>
2017-11-22 21:26:47 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,8 @@ def bonus
    }
   }
 
-  epic_tragedy[:montague][:hero][:name][:status] = "dead",
-  [:capulet][:heroine][:name][:status] = "dead"
+  epic_tragedy[:montague][:hero][:name][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:name][:status] = "dead"
 
 
 
